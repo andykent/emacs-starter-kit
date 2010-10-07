1a42fb5e45a028593b201a2139e35d8efda06f2c
Paul Ingles <paul@oobaloo.co.uk>
2010-10-07 20:44:09 +0100

    Few mods

---
+++ paul.el
@@ -0,0 +1,16 @@
+;; dir to store all extra extensions
+(setq vendor-dir (concat dotfiles-dir "/vendor"))
+
+;; add my vendor dir to load path
+(add-to-list 'load-path vendor-dir)
+
+(load "paul/defuns")
+(load "paul/global")
+(load "paul/bindings")
+(load "paul/rvm")
+(load "paul/ack")
+(load "paul/yasnippets")
+(load "paul/haml")
+(load "paul/textmate")
+;;(load "garethjones/linum")
+(load "paul/elein")
